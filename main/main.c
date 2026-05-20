8a2b7c438a536369aa8c2eba986dde80aa2f4407
Eduardo Ramos <ingenieria@lctdidacticos.com>
2026-05-20 19:40:42 +0000

    cambios

--- main/main.c
+++ main/main.c
@@ -16,8 +16,8 @@
 
 static const char *TAG = "EcoSensor";
 
-static const char *MDNS_HOSTNAME = "ecosensor02";
-static const char *AP_SSID = "EcoSensor-02";
+static const char *MDNS_HOSTNAME = "ecosensor01";
+static const char *AP_SSID = "EcoSensor-01";
 static const char *AP_PASS = "LCT3180940";
 
 #define LOG_EACH_SAMPLE          1
