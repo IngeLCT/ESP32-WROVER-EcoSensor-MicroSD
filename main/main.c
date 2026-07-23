4137e7910e8dfc0be0c4dd29fb543ee096c0d398
IngeLCT <ingenieria@lctdidacticos.com>
2026-07-23 12:41:55 -0600

    cambios

--- main/main.c
+++ main/main.c
@@ -29,7 +29,7 @@ static const char *AP_PASS = "LCTECO03";
 // Offset EcoSensor01: SCD40 = 7.70 SEN55 = -3.02
 // Offset EcoSensor02: SCD40 = 7.70 SEN55 = -3.02
 // Offset EcoSensor03: SCD40 calibrado; SEN55 conserva su compensación calibrada.
-const float ECO_SCD40_TEMP_OFFSET_C = 3.57f;
+const float ECO_SCD40_TEMP_OFFSET_C = 5.7f;
 const float ECO_SEN55_TEMP_OFFSET_C = -3.02f;
 
 #define LOG_EACH_SAMPLE          0
